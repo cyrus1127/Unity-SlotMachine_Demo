50763f982120182a3c8fd48bd1232312f6546e47
cyrus1127 <cyrus1127@gmail.com>
2020-12-03 10:37:04 +0000

    Update
    - Add model
    - Add textture
    - create script
    - create set of Animation
    - create anima controller

---
+++ Assets/Resources/InGame/ObjectAnims/RollingDice/RollingDiceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingDiceHelper : MonoBehaviour
+{
+    private Animator controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = this.GetComponent<Animator>();
+        SetResult(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetResult(int resultVal) {
+        if (resultVal >= 1 && resultVal <= 6) {
+            if (controller) {
+                controller.SetInteger("Result", resultVal);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        if (controller)
+        {
+            controller.SetTrigger("StartAnima");
+        }
+    }
+}
